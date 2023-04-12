ba05691e3e1764b5604abf54b4243f0e93787e0e
burnout131 <marcrg07@gmail.com>
2023-04-12 18:26:29 +0200

    DemoMavenApplication

---
+++ demoMaven/src/main/java/com/example/demo/DemoMavenApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMavenApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String welcome() {
+		return String.format("Welcome home!");
+	}
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return String.format("Hello world");
+	}
+	
+	@GetMapping("/person")
+	public String person(@RequestParam(value = "name", defaultValue = "person") String name) {
+		return String.format("Hello %s", name);
+	}
+
+}
